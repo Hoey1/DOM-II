affef91afc939eb0e887d278d776a1a152c03126
Joey <joey@xm8.ninja>
2020-05-05 17:32:04 -0400

    mouseover and mouseout done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+//?     --------------------------
+//?     -- mouseover & mouseout --
+//?     --------------------------
+
+const mouse = document.querySelector(".intro p");
+mouse.addEventListener("mouseover", mouseFunc, false);
+mouse.addEventListener("mouseout", mouseOutFunc, false);
+
+function mouseFunc() {
+  mouse.setAttribute("style", "color: orange;");
+}
+function mouseOutFunc() {
+  mouse.setAttribute("style", "color: black;");
+}
+
+// document.querySelectorAll('img')
